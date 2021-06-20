85c37d93d20cc58f2ff2379d0c974439af890830
Zoha Kaukab <56480034+zoha-ahref@users.noreply.github.com>
2021-06-20 07:30:04 -0400

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(newNote) {
+  return <Note title={newNote.title} content={newNote.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
